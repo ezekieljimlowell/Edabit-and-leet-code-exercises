36e3d3182c67ce3a47fc8cdea54ebde6ed2d1b3e
ezekieljimlowell <ezekiel@aptonworks.com>
2026-05-13 07:31:22 +0530

    solution twosum

--- Leetcode exercises/twoSum.js
+++ Leetcode exercises/twoSum.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-
+    let length = nums.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; i < length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+            else {
+                return false;
+            }
+        }
+    }
 };
+
+console.log(twoSum([1, 2, 3, 4, 5], 7))
